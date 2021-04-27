99a3f0d0f1a4c19e13958ccbc8fee86459864823
Jesus Rodriguez <jesus.rodrz97@gmail.com>
2021-04-26 21:39:28 -0400

    fix pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,14 +1,19 @@
 // @flow
 
-import React from 'react';
-import memoize from 'lodash/memoize';
-import { css } from '@emotion/core';
+import React from "react";
+import memoize from "lodash/memoize";
+import { css } from "@emotion/core";
 
-import { Select } from '../Select';
-import { Button } from '../Button';
-import { Icon } from '../Icon';
-import { Text } from '../Text';
-import { theme, PaginationTag, PaginationItemsTag, PaginationItemTag } from './Pagination.theme';
+import { Select } from "../Select";
+import { Button } from "../Button";
+import { Icon } from "../Icon";
+import { Text } from "../Text";
+import {
+  theme,
+  PaginationTag,
+  PaginationItemsTag,
+  PaginationItemTag,
+} from "./Pagination.theme";
 
 type PaginationProps = {
   page?: number,
@@ -28,7 +33,10 @@ type PaginationState = {
 
 const VISIBLE_RANGE = 2;
 
-const formatter = new Intl.NumberFormat('en-EN', { useGrouping: true, maximumFractionDigits: 0 });
+const formatter = new Intl.NumberFormat("en-EN", {
+  useGrouping: true,
+  maximumFractionDigits: 0,
+});
 
 class Pagination extends React.Component<PaginationProps, PaginationState> {
   static defaultProps = {
@@ -49,7 +57,7 @@ class Pagination extends React.Component<PaginationProps, PaginationState> {
   }
 
   // $FlowFixMe
-  createOnChange = memoize(page => () => {
+  createOnChange = memoize((page) => () => {
     const { onChange, pageSize: pageSizeFormProps } = this.props;
     const { pageSize: pageSizeFromState } = this.state;
 
@@ -57,7 +65,7 @@ class Pagination extends React.Component<PaginationProps, PaginationState> {
 
     this.setState(() => ({ page }));
 
-    if (typeof onChange === 'function') {
+    if (typeof onChange === "function") {
       onChange(page, pageSize);
     }
   });
@@ -72,16 +80,23 @@ class Pagination extends React.Component<PaginationProps, PaginationState> {
 
     this.setState(() => ({ pageSize, page: nextPage }));
 
-    if (typeof onChange === 'function') {
+    if (typeof onChange === "function") {
       onChange(nextPage, pageSize);
     }
   };
 
   // $FlowFixMe
-  getPageSizeOptions = (pageSizeOptions: number[]) => pageSizeOptions.map(value => ({ value, label: String(value) }));
+  getPageSizeOptions = (pageSizeOptions: number[]) =>
+    pageSizeOptions.map((value) => ({ value, label: String(value) }));
 
   render() {
-    const { page: pageFromProps, pageSize: pageSizeFormProps, total, pageSizeOptions = [], showSizeChanger } = this.props;
+    const {
+      page: pageFromProps,
+      pageSize: pageSizeFormProps,
+      total,
+      pageSizeOptions = [],
+      showSizeChanger,
+    } = this.props;
     const { page: pageFromState, pageSize: pageSizeFromState } = this.state;
 
     const page = pageFromProps || pageFromState;
@@ -89,7 +104,10 @@ class Pagination extends React.Component<PaginationProps, PaginationState> {
 
     const numberOfPages = Math.ceil(total / pageSize);
 
-    const leftSide = Math.min(page - VISIBLE_RANGE, numberOfPages - VISIBLE_RANGE * 2);
+    const leftSide = Math.min(
+      page - VISIBLE_RANGE,
+      numberOfPages - VISIBLE_RANGE * 2
+    );
     const rightSide = page + VISIBLE_RANGE;
 
     const showLeftMore = leftSide > 2;
@@ -97,7 +115,10 @@ class Pagination extends React.Component<PaginationProps, PaginationState> {
 
     const start = leftSide > 1 ? leftSide : 1;
 
-    const pages = [...Array(VISIBLE_RANGE * 2 + 1).keys()].slice(0, numberOfPages);
+    const pages = [...Array(VISIBLE_RANGE * 2 + 1).keys()].slice(
+      0,
+      numberOfPages
+    );
 
     const firstRecordIndex = (page - 1) * pageSize + 1;
     const lastRecordIndex = Math.min(total, page * pageSize);
@@ -106,85 +127,97 @@ class Pagination extends React.Component<PaginationProps, PaginationState> {
       <PaginationTag tagName="div">
         <Button
           size="sm"
-          css={ css`
+          css={css`
             flex-shrink: 0;
-          ` }
-          onClick={ this.createOnChange(page - 1) }
+          `}
+          onClick={this.createOnChange(page - 1)}
           color="neutral"
           variant="outlined"
           squared
-          disabled={ page <= 1 }
-          withIconAutosize={ false }
+          disabled={page <= 1}
+          withIconAutosize={false}
         >
           <Icon name="ChevronLeft" size="xs" />
         </Button>
 
         <PaginationItemsTag>
-          <If condition={ leftSide > 1 }>
-            <PaginationItemTag active={ page === 1 } onClick={ this.createOnChange(1) }>
+          <If condition={leftSide > 1}>
+            <PaginationItemTag
+              active={page === 1}
+              onClick={this.createOnChange(1)}
+            >
               1
             </PaginationItemTag>
           </If>
 
-          <If condition={ showLeftMore }>
+          <If condition={showLeftMore}>
             <PaginationItemTag disabled>
               <Icon name="More" />
             </PaginationItemTag>
           </If>
 
-          { pages.map(index => (
-            <PaginationItemTag key={ index + start } active={ index + start === page } onClick={ this.createOnChange(index + start) }>
-              { index + start }
+          {pages.map((index) => (
+            <PaginationItemTag
+              key={index + start}
+              active={index + start === page}
+              onClick={this.createOnChange(index + start)}
+            >
+              {index + start}
             </PaginationItemTag>
-          )) }
+          ))}
 
-          <If condition={ showRightMore }>
+          <If condition={showRightMore}>
             <PaginationItemTag disabled>
               <Icon name="More" />
             </PaginationItemTag>
           </If>
 
-          <If condition={ rightSide < numberOfPages }>
-            <PaginationItemTag active={ page === numberOfPages } onClick={ this.createOnChange(numberOfPages) }>
-              { numberOfPages }
+          <If condition={rightSide < numberOfPages && showRightMore}>
+            <PaginationItemTag
+              active={page === numberOfPages}
+              onClick={this.createOnChange(numberOfPages)}
+            >
+              {numberOfPages}
             </PaginationItemTag>
           </If>
         </PaginationItemsTag>
 
         <Button
           size="sm"
-          css={ css`
+          css={css`
             flex-shrink: 0;
-          ` }
-          onClick={ this.createOnChange(page + 1) }
+          `}
+          onClick={this.createOnChange(page + 1)}
           color="neutral"
           variant="outlined"
           squared
-          disabled={ page >= numberOfPages }
-          withIconAutosize={ false }
+          disabled={page >= numberOfPages}
+          withIconAutosize={false}
         >
           <Icon name="ChevronRight" size="xs" />
         </Button>
 
-        <If condition={ !!showSizeChanger }>
+        <If condition={!!showSizeChanger}>
           <Select
-            css={ css`
+            css={css`
               width: 64px;
               margin-left: 12px;
-            ` }
-            value={ pageSize }
-            options={ this.getPageSizeOptions(pageSizeOptions) }
-            onChange={ this.onChangePageSize }
-            isSearchable={ false }
+            `}
+            value={pageSize}
+            options={this.getPageSizeOptions(pageSizeOptions)}
+            onChange={this.onChangePageSize}
+            isSearchable={false}
           />
         </If>
 
         <Text
-          css={ css`
+          css={css`
             margin-left: 12px;
-          ` }
+          `}
         >
-          { formatter.format(firstRecordIndex) } - { formatter.format(lastRecordIndex) } of { formatter.format(total) } records
+          {formatter.format(firstRecordIndex)} -{" "}
+          {formatter.format(lastRecordIndex)} of {formatter.format(total)}{" "}
+          records
         </Text>
       </PaginationTag>
     );
